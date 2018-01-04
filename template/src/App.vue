50a232691152e37993f449c67fe358f66192bf9b
Pablo Ramirez <pabloandres2700@gmail.com>
2018-01-04 15:08:25 -0500

    Parcel vue

---
+++ template/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>{{text}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			text: 'Parcel Vue App'
+		};
+	}
+};
+</script>
+
+<style {{#sass}} lang="scss" {{/sass}}>
+body {
+	font-family: sans-serif;
+}
+h1 {
+	color: #444;
+}
+</style>
